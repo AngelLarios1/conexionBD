ac1451c849172c69d23e5582d763ec83ceca0451
Brandon Axel Alonso López <160175034+Brandon030605@users.noreply.github.com>
2026-09-18 18:57:22 -0600

    local we

--- dbcon.php
+++ dbcon.php
@@ -7,12 +7,12 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-$host     = $_ENV['DB_HOST'] ?? '127.0.0.1';
-$port     = $_ENV['DB_PORT'] ?? '3306';
-$db       = $_ENV['DB_NAME'] ?? '';
-$user     = $_ENV['DB_USER'] ?? 'root';
-$pass     = $_ENV['DB_PASS'] ?? '';
-$charset  = 'utf8mb4';
+$host = $_ENV['DB_HOST'] ?? '127.0.0.1';
+$port = $_ENV['DB_PORT'] ?? '3306';
+$db = $_ENV['DB_NAME'] ?? 'ecommerce';
+$user = $_ENV['DB_USER'] ?? 'root';
+$pass = $_ENV['DB_PASS'] ?? '';
+$charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
